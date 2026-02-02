3a1b1917cc9b7f55d54dfaf4c304b88937485f54
wessel05j <ejwessel05@gmail.com>
2026-02-02 13:33:34 +0100

    AppTest testing all avilable functionality

--- textmanipulation/src/test/java/no/ntnu/idatg2003/AppTest.java
+++ textmanipulation/src/test/java/no/ntnu/idatg2003/AppTest.java
@@ -1,20 +1,36 @@
 package no.ntnu.idatg2003;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testReplaceTextCommand() {
+        ReplaceTextCommand cmd = new ReplaceTextCommand("cat", "dog");
+        assertEquals("The dog sat on the mat.", cmd.execute("The cat sat on the mat."));
+    }
+
+    @Test
+    public void testReplaceFirstTextCommand() {
+        ReplaceFirstTextCommand cmd = new ReplaceFirstTextCommand("cat", "dog");
+        assertEquals("dog cat cat", cmd.execute("cat cat cat"));
+    }
+
+    @Test
+    public void testWrapTextCommand() {
+        WrapTextCommand cmd = new WrapTextCommand("<b>", "</b>");
+        assertEquals("<b>hello</b>", cmd.execute("hello"));
+    }
+
+    @Test
+    public void testWrapLinesTextCommand() {
+        WrapLinesTextCommand cmd = new WrapLinesTextCommand("<li>", "</li>");
+        String input = "a\nb\nc";
+        String expected = "<li>a</li>\n<li>b</li>\n<li>c</li>";
+        assertEquals(expected, cmd.execute(input));
     }
 }
