d753d8aa75bae518aa08df9cd0fbdac778c1dc6b
wessel05j <ejwessel05@gmail.com>
2026-02-10 12:23:46 +0100

    Added MVP testing in AppTest.java

--- textmanipulation/src/test/java/no/ntnu/idatg2003/AppTest.java
+++ textmanipulation/src/test/java/no/ntnu/idatg2003/AppTest.java
@@ -8,29 +8,61 @@ import org.junit.Test;
  */
 public class AppTest {
 
-    @AppTest
+    @Test
     public void testReplaceTextCommand() {
         ReplaceTextCommand cmd = new ReplaceTextCommand("cat", "dog");
         assertEquals("The dog sat on the mat.", cmd.execute("The cat sat on the mat."));
     }
 
-    @AppTest
+    @Test
     public void testReplaceFirstTextCommand() {
         ReplaceFirstTextCommand cmd = new ReplaceFirstTextCommand("cat", "dog");
         assertEquals("dog cat cat", cmd.execute("cat cat cat"));
     }
 
-    @AppTest
+    @Test
     public void testWrapTextCommand() {
         WrapTextCommand cmd = new WrapTextCommand("<b>", "</b>");
         assertEquals("<b>hello</b>", cmd.execute("hello"));
     }
 
-    @AppTest
+    @Test
     public void testWrapLinesTextCommand() {
         WrapLinesTextCommand cmd = new WrapLinesTextCommand("<li>", "</li>");
         String input = "a\nb\nc";
         String expected = "<li>a</li>\n<li>b</li>\n<li>c</li>";
         assertEquals(expected, cmd.execute(input));
+    }
+
+    @Test
+    public void testWrapSelectionTextCommand() {
+        WrapSelectionTextCommand cmd = new WrapSelectionTextCommand("[", "]", "cat");
+        assertEquals("the [cat] sat", cmd.execute("the cat sat"));
+    }
+
+    @Test
+    public void testCapitalizeTextCommand() {
+        CapitalizeTextCommand cmd = new CapitalizeTextCommand();
+        assertEquals("Hello", cmd.execute("hello"));
+    }
+
+    @Test
+    public void testCapitalizeWordsTextCommand() {
+        CapitalizeWordsTextCommand cmd = new CapitalizeWordsTextCommand();
+        assertEquals("Hello World", cmd.execute("hello world"));
+    }
+
+    @Test
+    public void testCapitalizeSelectionTextCommand() {
+        CapitalizeSelectionTextCommand cmd = new CapitalizeSelectionTextCommand("fox");
+        assertEquals("the Fox jumps", cmd.execute("the fox jumps"));
+    }
+
+    @Test
+    public void testScriptCommand() {
+        Script script = new Script();
+        script.addCommand(new ReplaceTextCommand("dog", "cat"));
+        script.addCommand(new CapitalizeWordsTextCommand());
+        assertEquals("The Cat", script.execute("the dog"));
     }
 }
